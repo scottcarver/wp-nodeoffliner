31e97398e6eb9a5dcabfdb1fd284857242b2c4f7
Scott Carver <scottcarver@gmail.com>
2021-10-27 14:37:29 -0700

    Minor repairs to offlining process

--- index.js
+++ index.js
@@ -4,55 +4,69 @@ const rateLimit = require('axios-rate-limit');
 const auth = { username: 'fakeuser', password: 'fakepass' };
 const throttledaxios = rateLimit(axios.create(), { maxRequests: 10, perMilliseconds: 1000, maxRPS: 10 });
 const remotebase = 'https://edit.pointoforegon.com';
-const localbase = 'http://localhost/wp-pointoforegon/';
-const activebase = remotebase;
-const activeroute = activebase + '/changelog/in-out.json';
+const localbase = 'http://localhost/gutenberg-test/';
+const activebase = localbase;
+const activeroute = activebase + 'route/manifest.json';
+const path = require('path');
 
 getChanges();
 
 async function getChanges(){
+    console.log(activeroute);
     // Remove /public/ cache
-    fs.rmdir('public', { recursive: true }, (err) => {if (err) {throw err;} });
-    // Create /public/ fresh
-    fs.mkdir('public', badoop); // (err) => {if (err) {throw err;} }  
+    fs.rmdir('public', { recursive: true }, (err) => {
+        if (err) {
+          return console.error(err);
+        }
+        // console.log('Directory created successfully!');
+         // Create /public/ fresh
+        fs.mkdir(path.join(__dirname, 'public'),{ recursive: true }, (err) => {if (err) {throw err;} });
+    });
+   
     // Changelog URL
     // Retrieve Changelog
-    const data = await axios.get(activeroute, {auth}).then(function (response) {   
+    const data = await axios.get(activeroute, {auth}).then(function (response) { 
+        console.log(response.data);  
         // Write Changelog
         fs.writeFile("public/changelog.json", JSON.stringify(response.data), (err) => {if (err) { throw err;} });  
         // Write Resources // .permalinks.slice(0, 3)
-        getResource(str);
+
+        response.data.forEach( function(str) {
+            var filename =  str.split('/').pop();
+            console.log('Downloading ' + str);
+            getResource(str); // , filename, function(){console.log('Finished Downloading' + filename)
+        });
+
+       // getResource(str);
     }).catch((error) => {
         console.log(error);
     });
 };
 
+// Get a Resource
 async function getResource(src){
+
     const data = await throttledaxios.get(src, {auth}).then(function (response) { 
-        
-        // console.log('start write');
         // Make Directory
         fs.mkdirSync(cleanpath(src), { recursive: true });
         // Make File
         fs.writeFile(cleanpath(src)+'index.html', JSON.parse(JSON.stringify(response.data)), (err) => {if (err) {throw err;} });
         console.log('created ' + cleanpath(src));
-    
         
     }).catch((error) => {
         console.log(error);
     });
   
     return false;
-  };
+};
 
 
-function badoop(){
-    return false;
-}
 
+// Clean the path by removing the URL base
 function cleanpath(path){
+    // Remove the base
     const itcleaned = path.replace(activebase, 'public/');
-    // console.log('clean ise ' + itcleaned);
+    // Return the url minus the base
     return itcleaned;
 }
 
