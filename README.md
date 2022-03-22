03a6939e774e3cb3a6eb5c541c9d178b0a404c38
Scott Carver <scottcarver@gmail.com>
2022-03-22 10:32:33 -0700

    New logic for files and support directories

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # wp-nodeoffliner
-Node script to offload some WordPress URIs
+Node script to offload a list of urls. For practical purposes they will likely be WordPress URIs, but the solution is unopinionated on the backend since it scrapes the rendered code. The file is a flat array of urls for which to offload. The technique is 1) useful to offload a website, and also 2) to offline pages inside a PWA. 
+
+## Feed Examples
+- `/app/data-alltime.js` - a full list of all site URI's
+- `/app/data-since233113.js` - a list of site changes  since a specific isotime. This is used in the `WP_Query` to be economical about only getting URIs that changed since the last sync. 
+
+## Example JSON Data:
+``{
+    [
+        url1,
+        url2,
+        url3,
+        folder1/url4,
+    ]
+}``
+
+## Static Files
+**This will create 4 files:**
+
+1) `url1/index.html`
+2) `url2/index.html`
+3) `url3/index.html`
+4) `folder1/url4/index.html`
+
+Notice how folders are created automatically, and index.html files are created to create the illusion of clean folder structure.
+
+To run the script:
+1) npm install
+2) npm start
--- index.js
+++ index.js
@@ -1,40 +1,60 @@
+// Packages
 const fs = require('fs');
 const axios = require("axios");
 const rateLimit = require('axios-rate-limit');
+// Variables
 const auth = { username: 'fakeuser', password: 'fakepass' };
 const throttledaxios = rateLimit(axios.create(), { maxRequests: 10, perMilliseconds: 1000, maxRPS: 10 });
 const remotebase = 'https://edit.pointoforegon.com';
 const localbase = 'http://localhost/gutenberg-test/';
 const activebase = localbase;
-const activeroute = activebase + 'route/manifest.json';
+const activeroute = activebase + 'app/index.json';
 const path = require('path');
 
 getChanges();
 
 async function getChanges(){
     console.log(activeroute);
+
     // Remove /public/ cache
-    fs.rmdir('public', { recursive: true }, (err) => {
-        if (err) {
-          return console.error(err);
-        }
-        // console.log('Directory created successfully!');
-         // Create /public/ fresh
-        fs.mkdir(path.join(__dirname, 'public'),{ recursive: true }, (err) => {if (err) {throw err;} });
+    fs.rm('public', { recursive: true }, (err) => {
+        // Return, if error
+        if (err) {  return console.error(err); }
+        // Create /public/ fresh
+        fs.mkdir(path.join(__dirname, 'public/'),{ recursive: true }, (err) => { if (err) {throw err;} });
+        
+       
+        /*
+         // Create Changelog file
+         fs.writeFile("app/changelog.json", "data",
+         {
+             encoding: "utf8",
+             flag: "w",
+             mode: 0o666
+         },
+         (err) => {
+             if (err)
+             console.log(err);
+             else {
+             console.log("File written successfully\n");
+             console.log("The written has the following contents:");
+             console.log(fs.readFileSync("app/changelog.json", "utf8"));
+             }
+         });
+         */
     });
    
-    // Changelog URL
     // Retrieve Changelog
     const data = await axios.get(activeroute, {auth}).then(function (response) { 
         console.log(response.data);  
         // Write Changelog
-        fs.writeFile("public/changelog.json", JSON.stringify(response.data), (err) => {if (err) { throw err;} });  
+       // fs.writeFile("public/changelog.json", JSON.stringify(response.data), (err) => {if (err) { throw err;} });  
         // Write Resources // .permalinks.slice(0, 3)
 
-        response.data.forEach( function(str) {
+        response.data.urls.forEach( function(str) {
             var filename =  str.split('/').pop();
-            console.log('Downloading ' + str);
-            getResource(str); // , filename, function(){console.log('Finished Downloading' + filename)
+            console.log('Downloading ' + response.data.base + str);
+            getResource(response.data.base + str); // , filename, function(){console.log('Finished Downloading' + filename)
         });
 
        // getResource(str);
@@ -46,12 +66,50 @@ async function getChanges(){
 // Get a Resource
 async function getResource(src){
 
-    const data = await throttledaxios.get(src, {auth}).then(function (response) { 
-        // Make Directory
-        fs.mkdirSync(cleanpath(src), { recursive: true });
-        // Make File
-        fs.writeFile(cleanpath(src)+'index.html', JSON.parse(JSON.stringify(response.data)), (err) => {if (err) {throw err;} });
-        console.log('created ' + cleanpath(src));
+    const data = await throttledaxios.get(src, {auth}).then(function (response) {
+        var lastChar = src.charAt(src.length - 1);
+        var hasTrailingSlash = lastChar == '/';
+        var split = cleanpath(src).split('/');
+        var minibase = '';
+        var filename = ''
+
+       // console.log("split:" + split + " and length is:" + split.length);
+        // console.log(src + ' Traling slash is ' + lastChar + hasTrailingSlash);
+        
+        // Clean Directories
+       if(hasTrailingSlash){
+            // Make Directory
+            fs.mkdirSync(cleanpath(src), { recursive: true });
+            // Make File
+            fs.writeFile(cleanpath(src)+'index.html', JSON.parse(JSON.stringify(response.data)), (err) => {if (err) {throw err;} else{ 
+                console.log("✨ created a new html view at " + cleanpath(src));} });
+       }
+
+       
+        // Single Files
+        if(!hasTrailingSlash){
+
+            // Creat a new base, minus the file!
+            for (let i = 0; i < split.length - 1; i++) { minibase += split[i] + '/'; }
+
+            // Check if the Support Directory exists
+            if(!fs.existsSync(cleanpath(minibase))){
+                fs.mkdirSync(cleanpath(minibase), { recursive: true });
+                console.log('📁 created a new directory at ' + minibase);
+            }
+           
+            // Encode, if JSON
+            if(typeof response.data  === 'object'){ response.data = JSON.stringify(response.data);  }
+
+            // Write the file, now that the folder exists
+            fs.writeFile(cleanpath(src), response.data, (err) => {
+                if (err) console.log(err); else { console.log("✅ created a new file at " + cleanpath(src));}
+            });
+
+        } 
+      
+       
+   
         
     }).catch((error) => {
         console.log(error);
